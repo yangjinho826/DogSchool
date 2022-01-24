75315552e1874b0915d9fe306378bfd25fd2e04e
JAE YOON SONG <jaeyun115@naver.com>
2022-01-24 16:56:52 +0900

    add one last time

--- src/main/java/com/dog/HC/HomeController.java
+++ src/main/java/com/dog/HC/HomeController.java
@@ -1,37 +1,9 @@
 package com.dog.HC;
 
-<<<<<<< HEAD
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.dog.HC.member.MemberDAO;
-
-@Controller
-public class HomeController {
-	
-	@Autowired
-	private MemberDAO mDAO;
-	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(HttpServletRequest req) {
-		
-		mDAO.loginCheck(req);
-
-=======
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
