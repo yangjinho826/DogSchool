21f8dfa2b15ff8a139ffd08b026e8d8da7e938fd
yangjinho <yangjinho826@naver.com>
2022-01-27 20:48:03 +0900

    fix1-schoolmain

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -12,6 +12,7 @@ create table signup_table(
 select * from signup_table 
 insert into signup_table values('root','1234','관리자','00000000000','남',0)
 
+
 -- 공지 테이블 --
 create table notice_table(
 	n_no number(3) primary key,
@@ -21,7 +22,7 @@ create table notice_table(
 	n_txt varchar2(4000 char) not null,
 	n_date date not null
 );
-create sequence notice_table_seq;
+create sequence notice_seq;
 
 -- 선생님 후기 테이블 --
 create table postscript_table(
@@ -33,7 +34,7 @@ create table postscript_table(
 	p_date date not null
 	
 );
-create sequence postscript_table_seq;
+create sequence postscript_seq;
 
 -- 가격표 테이블
 create table priceTag_table(
@@ -44,7 +45,7 @@ create table priceTag_table(
 	p_date number(10) not null
 	
 );
-create sequence priceTag_table_seq;
+create sequence priceTag_seq;
 
 -- 스케줄 테이블
 create table schedule_table(
@@ -54,9 +55,9 @@ create table schedule_table(
 	s_text varchar2(30 char) not null,	
 	s_day date not null
 );
+create sequence schedule_seq;
 
-create sequence schedule_table_seq;
-select * from schedule_table
+
 -- 원장-관리자 신청 테이블 --
 create table Dapply_table(
     Da_no number(5) primary key, -- 유치원 구분
